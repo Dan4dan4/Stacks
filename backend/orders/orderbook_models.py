b01afdceb466e03699c930a1ee54ad108a5063a2
Danish <danm44@hotmail.com>
2026-03-05 12:50:59 -0500

    pricelevel doubly linkedlist

--- backend/orders/orderbook_models.py
+++ backend/orders/orderbook_models.py
@@ -1,7 +1,10 @@
 import time
 
-# represents a single tick or Order
+
 class OrderNode:
+    """
+    represents a single ORDER or "tick
+    """
     def __init__(self, order_id, price, quantity, user_id, side):
         self.order_id = order_id
         self.price = price
@@ -13,3 +16,15 @@ class OrderNode:
         # pointers for the doubly linkedlist
         self.next = None
         self.prev = None
+
+
+class PriceLevelList:
+    """
+    represents all orders at a specific price. this is going to create a forest of Doubly linked lists, 
+    every diff price level for bids and asks is going to hav their own DLL.
+    """
+    def __init__(self):
+
+        self.head= None
+        self.tail = None
+        self.total_volume = 0
