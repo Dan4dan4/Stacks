06446b22022568e07eb1697c7cbbc2e5271e4998
Danish <danm44@hotmail.com>
2026-03-08 17:25:02 -0400

    feedhandler

--- backend/feed_handler/feed_handler.py
+++ backend/feed_handler/feed_handler.py
@@ -6,4 +6,7 @@ class FeedHandler:
         self.symbols = symbols
 
         self.url = "wss://delayed.massive.com/stocks"
+
+        # creates the forest of orderbooks, using a dictionary because we WONT be foresting all stocks from this websocket
+        # we will populate the dictionary and use it for this specific demo
         self.books = {s: OrderBook(s) for s in symbols}
